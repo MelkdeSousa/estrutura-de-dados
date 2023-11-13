02d6affac207f96653b0aaff2b4325abe0c0dd42
Melk de Sousa <45469576+MelkdeSousa@users.noreply.github.com>
2023-11-13 16:22:14 +0000

    stack

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/melkdesousa/estrutura-de-dados
+
+go 1.21.3
---
+++ stack.go
@@ -0,0 +1,22 @@
+package estruturadedados
+
+import "errors"
+
+type Stack[T any] []T
+
+func (s *Stack[T]) Push(valor T) {
+    *s = append(*s, valor)
+}
+
+func (s *Stack[T]) Pop() (T, error) {
+    if len(*s) == 0 {
+        return T{}, errors.New("empty stack")
+    }
+    topo := (*s)[len(*s)-1]
+    *s = (*s)[:len(*s)-1]
+    return topo, nil
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+    return len(*s) <= 0
+}
