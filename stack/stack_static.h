0678f7a6982b73f6646ff5386edb78a00a3122bb
Melk de Sousa <melk.sousap@gmail.com>
2023-11-18 16:38:19 -0300

    format file

--- stack/stack_static.h
+++ stack/stack_static.h
@@ -1,6 +1,6 @@
 #define MAX_ITEMS 100
 
-typedef struct 
+typedef struct
 {
     int data[MAX_ITEMS];
     int top;
@@ -9,7 +9,6 @@ typedef struct
 void create(StackStatic *stack);
 void push(StackStatic *stack, int element);
 int pop(StackStatic *stack);
-int isEmpty(StackStatic *stack);
-int isFull(StackStatic *stack);
-int peek(StackStatic *stack);
 
+int isEmpty(StackStatic stack);
+int isFull(StackStatic stack);
