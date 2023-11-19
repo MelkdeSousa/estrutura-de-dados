5bb15719540bd3aa58edca0cdc69913a7624b81f
Melk de Sousa <melk.sousap@gmail.com>
2023-11-19 16:31:01 -0300

    add simply linked list

---
+++ structures/simply_linked_list/node.go
@@ -0,0 +1,30 @@
+package simplylinkedlist
+
+
+type Node[T any] struct {
+	Value T
+	Next  *Node[T] // pointer to the next node
+}
+
+func NewNode[T any](value T) *Node[T] {
+	return &Node[T]{
+		Value: value,
+		Next:  nil,
+	}
+}
+
+func (node *Node[T]) GetValue() T {
+	return node.Value
+}
+
+func (node *Node[T]) SetValue(value T) {
+	node.Value = value
+}
+
+func (node *Node[T]) GetNext() *Node[T] {
+	return node.Next
+}
+
+func (node *Node[T]) SetNext(next *Node[T]) {
+	node.Next = next
+}
---
+++ structures/simply_linked_list/simply_linked_list.go
@@ -0,0 +1,69 @@
+package simplylinkedlist
+
+import (
+	"errors"
+	"fmt"
+)
+
+type SimplyLinkedList[T any] struct {
+	Head *Node[T]
+}
+
+func NewSimplyLinkedList[T any]() *SimplyLinkedList[T] {
+	return &SimplyLinkedList[T]{
+		Head: nil,
+	}
+}
+
+func (list *SimplyLinkedList[T]) IsEmpty() bool {
+	return list.Head == nil
+}
+
+func (list *SimplyLinkedList[T]) Push(value T) {
+	node := NewNode(value)
+
+	if list.IsEmpty() {
+		list.Head = node
+		return
+	}
+
+	current := list.Head
+
+	for current.GetNext() != nil {
+		current = current.GetNext()
+	}
+
+	current.SetNext(node)
+}
+
+func (list *SimplyLinkedList[T]) Pop() (T, error) {
+	if list.IsEmpty() {
+		var v T
+
+		return v, errors.New("empty list")
+	}
+
+	current := list.Head
+
+	element := current.GetValue()
+
+	list.Head = current.GetNext()
+
+	return element, nil
+}
+
+func (list SimplyLinkedList[T]) String() string {
+	if list.Head == nil {
+		return ""
+	}
+
+	current := list.Head
+	result := "[ "
+
+	for current != nil {
+		result += fmt.Sprintf("%v ", current.Value)
+		current = current.GetNext()
+	}
+
+	return result + "]"
+}
