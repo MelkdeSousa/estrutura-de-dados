9e6ee7cca15a839a634b6f7657e5dd64bd1802e1
Melk de Sousa <melk.sousap@gmail.com>
2023-11-20 22:20:02 -0300

    converter decimal to binary with dynamic stack

--- stack/examples/super_dec_to_bin.c
+++ stack/examples/super_dec_to_bin.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "stack_static.h"
+#include "../stack_dynamic.h"
 
 int main(int argc, char const *argv[])
 {
     int value, rest;
 
-    StackStatic stack;
-
-    create(&stack);
+    DynamicStack *stack = createStack();
 
     printf("Enter a decimal number: ");
 
@@ -16,15 +14,15 @@ int main(int argc, char const *argv[])
     while (value > 0)
     {
         rest = value % 2;
-        push(&stack, rest);
+        push(stack, rest);
         value = value / 2;
     }
 
     printf("Binary number: ");
 
-    while (!isEmpty(stack))
+    while (!isEmpty(*stack))
     {
-        printf("%d", pop(&stack));
+        printf("%d", pop(stack));
     }
 
     printf("\n");
